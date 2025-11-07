26772f41652770fd2026894c2ba19da8f06a61b2
Ezequiel Alves Soares <Ezequiel-Alves@users.noreply.huggingface.co>
2025-11-07 16:35:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -551,4 +551,9 @@ pn.config.raw_css.append(custom_css)
 template.main.append(layout)
 template.servable()
 
+# no fim do arquivo:
+if __name__ == "__main__":
+    pn.serve(template, port=7860, show=False)
+else:
+    app = template
 
